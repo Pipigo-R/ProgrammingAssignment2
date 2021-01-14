e306b4a3232ee748e220bf7a2b14ac01ff41a486
Pipigo-R <47378333+Pipigo-R@users.noreply.github.com>
2021-01-13 21:35:53 -0800

    Add files via upload
    Complete implementation for the program of caching the inverse matrix

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,38 @@
-## Put comments here that give an overall description of what your
-## functions do
+### Assignment (WK2): Caching the Inverse of a Matrix
+### write an R function that is able to cache potentially time-consuming computations.
 
-## Write a short comment describing this function
+## makeCacheMatrix: This function creates a special "matrix" object that
+##                  can cache its inverse.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  i <- NULL
+  set <- function(y) {
+    x <<- y
+    i <<- NULL
+  }
+  get <- function() x
+  setminv <- function(inverse) i <<- inverse
+  getminv <- function() i
+  list(set = set, get = get,
+       setminv = setminv,
+       getminv = getminv)
 }
 
 
-## Write a short comment describing this function
+## cacheSolve: This function computes the inverse of the special "matrix" returned by
+##                makeCacheMatrix above. If the inverse has already been calculated (and the 
+##                matrix has not changed), then the cachesolve should retrieve the inverse 
+##                from the cache.
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  i <- x$getminv()
+  if(!is.null(i)) {
+    message("getting cached data")
+    return(i)
+  }
+  data <- x$get()
+  i <- solve(data, ...)
+  x$setminv(i)
+  i
 }
